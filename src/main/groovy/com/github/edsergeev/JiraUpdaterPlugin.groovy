9e43b01f5637b40115fd9948695ea62b31a7d0fa
EdSergeev <regedik@gmail.com>
2016-10-14 01:28:03 +0300

    first working plugin

--- src/main/groovy/com/github/edsergeev/JiraUpdaterPlugin.groovy
+++ src/main/groovy/com/github/edsergeev/JiraUpdaterPlugin.groovy
@@ -1,5 +1,17 @@
 package com.github.edsergeev
 
+import org.gradle.api.Plugin
+import org.gradle.api.Project
 
-class JiraUpdaterPlugin {
+
+class JiraUpdaterPlugin implements Plugin<Project> {
+    void apply(Project target) {
+        def showDevicesTask = target.tasks.create("showDevices") << {
+            def adbExe = target.android.getAdbExe().toString()
+            println "${adbExe} devices".execute().text
+        }
+
+        showDevicesTask.group = "jiraupdater"
+        showDevicesTask.description = "cool gradlw plugin for jira"
+    }
 }
